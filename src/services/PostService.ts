2f00433fdf45e2431129bc45eefd320716be1137
MacBook Pro <macbookpro@connector.local>
2021-05-06 09:41:17 +0100

    add postcontroller

---
+++ src/services/PostService.ts
@@ -0,0 +1,30 @@
+export class PostService 
+{
+    constructor()
+    {
+
+    }
+
+
+    public index()
+    {
+
+    }
+
+
+    public create()
+    {
+
+    }
+
+    public update()
+    {
+
+    }
+    
+    public delete()
+    {
+
+    }
+    
+}
