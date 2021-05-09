6b8e27f9ac0b731395667fb6afeefb579bd61d18
MacBook Pro <macbookpro@connector.local>
2021-05-09 19:54:55 +0100

    completed validation

--- src/validators/CustomFileValidation.ts
+++ src/validators/CustomFileValidation.ts
@@ -1,35 +1,30 @@
-import {  CustomValidator } from 'express-validator';
 
-export const isValidImage: CustomValidator = (value,filename) => 
+ export const validateSingleImage = (req:any) =>
  {
-    const acceptedFileTypes:Array<string> = ['image/jpeg','image/jpg','image/jpg'];
+   const acceptedFileTypes:Array<string> = ['image/jpeg','image/jpg','image/png'];
+   if(req.files[0]==undefined)
+   {
+    return false;
+   }
 
-     if(filename.req.files[0]==undefined)
-     {
-      return Promise.reject('file type is invalid');
-     }
 
- 
-    let mimeType:string = filename.req.files[0].mimetype;
-    let fileSizeLimit:number = 2 * 1024 *1024; //2mb in binary
-    let fileSize:number = filename.req.files[0].size;
-    /**
-     * check against mimetype
-     */
-     if(!acceptedFileTypes.includes(mimeType))
-     {
-        return Promise.reject('file type is invalid');
-     }      
-        /**
-         * check file size
-         */
-        if(fileSize > fileSizeLimit)
-        {
-            return Promise.reject('uploaded file is too large');
-        }
-        
-    
- };
-  
-
+  let mimeType:string = req.files[0].mimetype;
+  let fileSizeLimit:number = 2 * 1024 *1024; //2mb in binary
+  let fileSize:number = req.files[0].size;
+  /**
+   * check against mimetype
+   */
+   if(!acceptedFileTypes.includes(mimeType))
+   {
+      return false;
+   }      
+      /**
+       * check file size
+       */
+      if(fileSize > fileSizeLimit)
+      {
+          return false;
+      }
+        return true; 
+ }
 
